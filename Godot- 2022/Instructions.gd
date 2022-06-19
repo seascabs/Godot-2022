f2b8e1aa69d95b66c21b493fab14bb6c30f1c1ac
jasonmccollum <jasonmccollum1@gmail.com>
2022-06-18 20:11:06 -0700

    Adjust instructions

--- Godot- 2022/Instructions.gd
+++ Godot- 2022/Instructions.gd
@@ -2,13 +2,13 @@ extends Control
 
 var currentScreen = 1
 
-func _ready():
-	while currentScreen < 12:
-		yield(get_tree().create_timer(2.0), 'timeout')
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		next()
-	get_tree().change_scene("res://res/game/game.tscn")
-
 
 func next():
-	currentScreen += 1
-	get_node('Inst%s' % currentScreen).visible = true
+	if currentScreen < 12:
+		currentScreen += 1
+		get_node('Inst%s' % currentScreen).visible = true
+		return
+	get_tree().change_scene("res://res/game/game.tscn")
